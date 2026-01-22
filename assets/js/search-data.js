2cf8f77a5dc7efedc014b6cf3aa47f6fac4822e6
abdullahejazjanjua <abdullahejaz822@gmail.com>
2026-01-22 16:35:25 +0000

    Deploying to gh-pages from @ abdullahejazjanjua/abdullahejazjanjua.github.io@9f44429e86962456c4712bd8b0c42583a00e911e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-repositories",
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
           section: "Navigation",
